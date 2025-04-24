f6827884356d4c978b3d7dbd59e58b9ef9e46875
zhulfaliant01 <zhulfaliant@gmail.com>
2025-04-24 10:46:13 +0700

    update celery_app

--- celery_app.py
+++ celery_app.py
@@ -1,7 +1,7 @@
 from celery import Celery
 import scraper
 
-app = Celery('tasks', broker='redis://localhost:6379/0',
-    backend='redis://localhost:6379/0')
+app = Celery('tasks', broker='redis://172.19.3.236:6379/0',
+    backend='redis://172.19.3.236:6379/0')
 
 app.autodiscover_tasks(['scraper'])
